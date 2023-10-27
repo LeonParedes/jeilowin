e71402f169f66866ed146f59f0ad2d4a0c6fd73a
LeonParedes <a19490947@itmexicali.edu.mx>
2023-10-26 20:58:21 -0700

    Tarea26- ListTile

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -26,6 +26,15 @@ return ListView.builder(
 
   Widget menuList(BuildContext context, int index){
     final menuItem = AppMenuItems[index];
-    return Text(menuItem.title);
-  }
+    final colors = Theme.of(context).colorScheme;
+    return
+    ListTile(
+     leading: Icon(menuItem.icon, color: colors.primary),
+     trailing: Icon(Icons.arrow_forward_ios_outlined,color: colors.primary),
+     title: Text(menuItem.title),
+     subtitle: Text(menuItem.subTitle),
+     onTap: (){},
+     );
+
+     }
 } 
