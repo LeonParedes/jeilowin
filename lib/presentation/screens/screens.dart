d902757598f50dbdc8e529adc76821de1ae7a14e
LeonParedes <a19490947@itmexicali.edu.mx>
2023-11-09 19:42:12 -0800

    folders

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -2,3 +2,4 @@
 export 'package:jeilowin/presentation/screens/buttons/buttons_screen.dart';
 export 'package:jeilowin/presentation/screens/cards/cards_screen.dart';
 export 'package:jeilowin/presentation/screens/home/home_screen.dart';
+export 'package:widgets_app/screens/animated/animated_screen.dart';
